1bf1b862da0a0ae53f27ef737909a7a6fa4d0024
Fuyf <fuyunfeng@idcmatic.com>
2020-01-11 21:31:40 +0800

    首页 => 找合作

--- pages/search-cooperation/index.js
+++ pages/search-cooperation/index.js
@@ -5,43 +5,14 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = Page({
   data: {
-    items: [],
-    offsetTop: wx.DEFAULT_HEADER_HEIGHT,
-    imgHeight: parseInt(wx.WIN_WIDTH / 1125 * 628),
-    customStyle: {
-      'background-color': '#eee',
-      height: '46px',
-      'line-height': '46px'
-    },
-    searchText: '',
-    height: wx.DEFAULT_CONTENT_HEIGHT,
-    searchHeight: 38
+    searchText: ''
   },
-
-  onReady: function onReady() {
-    //首屏页面需要在mounted中延时才能调用框架api，其他页面不需要延时
-    // setTimeout(() => {
-    //   //关闭app启动图
-    //   ui.closeSplashscreen()
-    // }, 1000)
-    for (var i = 1; i <= 50; i++) {
-      this.data.items.push(i + ' - 内容文本');
-    }
-  },
-  goDetail: function goDetail() {
+  goDetail() {
     wx.navigateTo({
       url: '/pages/corporate-information/corporateInformation'
     });
   },
-  handleInput: function handleInput() {
-    console.log('input');
-  },
-  sliderShow: function sliderShow() {
-    this.isShow = true;
-  },
-  navHandle: function navHandle() {
-    wx.navigateTo({
-      url: '/pages/detail'
-    });
+  handleInput(data) {
+    console.log('input', data.detail);
   }
 });
