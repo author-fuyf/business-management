887fe8372a6617a8577d57004ab7e3b75201e527
Fuyf <fuyunfeng@idcmatic.com>
2020-01-20 16:27:15 +0800

    我的

--- pages/me/me.js
+++ pages/me/me.js
@@ -4,27 +4,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = Page({
-  data: {},
-
-  onReady: function onReady() {},
-  goPromte: function goPromte() {
-    wx.navigateTo({
-      url: '/pages/me/join-promote'
-    });
-  },
-  goResources: function goResources() {
-    wx.navigateTo({
-      url: '/pages/me/resources'
-    });
-  },
-  goMyRelease: function goMyRelease() {
-    wx.navigateTo({
-      url: '/pages/me/my-release'
-    });
-  },
-  goProcessing: function goProcessing() {
-    wx.navigateTo({
-      url: '/pages/me/processing'
-    });
+  data: {
+    imgUrl: '../../static/img/me-top-bg.jpg'
   }
 });
